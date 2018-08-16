368d846334324c054b2e70df0e6d1beed26f544d
Alexander Terp <alexander.terp@gmail.com>
2018-08-16 20:38:49 +1000

    Add camera rotation with mouse movement
    Unity editor includes public fields for sensitivity in both the x and y
    directions.

---
+++ Project1/Assets/Scripts/FlyingCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingCamera : MonoBehaviour
+{
+    public float xLookSensitivity;
+    public float yLookSensitivity;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    PerformCameraRotation();
+	}
+
+    private void PerformCameraRotation()
+    {
+        float xRotationAmount = Input.GetAxis("Mouse X") * xLookSensitivity;
+        float yRotationAmount = Input.GetAxis("Mouse Y") * yLookSensitivity;
+
+        transform.localEulerAngles += new Vector3(-yRotationAmount, xRotationAmount, 0) * Time.deltaTime;
+    }
+}
