db8d7f4969d3f0e6121cab9aba098b6cc00f8a33
Alexander Terp <alexander.terp@gmail.com>
2018-08-17 00:20:38 +1000

    Add the ability to toggle the cursor
    When Escape is pressed, the mouse no longer rotates the camera and
    instead moves a cursor on the screen. This allows the use of UI
    elements that may be added in the future.
    Also added a hint and in the bottom right to explain that Escape can
    toggle the UI. This hint is toggled along with the UI.

---
+++ Project1/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Public fields that will not appear in the inspector.
+
+    // An event that will be triggered each time the UI is toggled.
+    // Other classes may subscribe to it.
+    public static event Action<bool> uiToggleEvent;
+
+    // Public variabled to not appear in the inspector.
+    [HideInInspector]
+    public static bool uiEnabled = false;
+
+    // Update is called once per frame
+	void Update ()
+	{
+	    CheckUiToggle();
+	}
+
+    /**
+     * Called every frame, this method will check if the button for toggling
+     * the UI was called. If it was, trigger an event that other classes
+     * can see occur, if subscribed.
+     */
+    private static void CheckUiToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            uiEnabled = !uiEnabled;
+            uiToggleEvent(uiEnabled);
+        }
+    }
+}
---
+++ Project1/Assets/Scripts/ToggleWithUi.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleWithUi : MonoBehaviour
+{
+    // This can be set to true if this game object should be disabled
+    // when the UI is enabled, and visa versa.
+    public bool invert = false;
+
+	// Use this for initialization
+	void Start () {
+		// Subscribe to the toggle event.
+	    GameController.uiToggleEvent += OnUiToggle;
+	}
+
+    /**
+     * A delegate that gets called when the Ui is toggled in GameController.
+     * Toggles the game object as required.
+     */
+    private void OnUiToggle(bool uiEnabled)
+    {
+        gameObject.SetActive(uiEnabled ^ invert);
+    }
+}
