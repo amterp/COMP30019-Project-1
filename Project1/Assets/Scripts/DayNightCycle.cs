2d7245c0ec99ce226badaa08537ce96a9a14b44a
Alexander Terp <alexander.terp@gmail.com>
2018-08-19 00:25:57 +1000

    Add incomplete day/night cycle
    Implements a day/night cycle with two light sources - a sun and a moon.
    On default, it goes at real time, though this can currently be changed
    via the public fields in the DayNightCycle script. I've played around a
    lot with the lighting values so it looks decent during both day and
    night.
    The reason why this is still incomplete is because, as demonstrated by
    the scene on load, some unwanted shadows appear where the sun, when on
    the horizon, can shine through the terrain, shadowing it with jaggies.
    See for yourself. This will need a fix, or the day/night cycle will have
    to be scrapped, unfortunately.

---
+++ Project1/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public float timeProgression;
+    public int startingHour;
+    public int startingMinute;
+    public int startingSecond;
+    public bool startAtCurrentTime;
+
+    private const int SecondsPerMinute = 60;
+    private const int MinutesPerHour = 60;
+    private const int HoursPerDay = 24;
+    private const int SecondsPerDay = SecondsPerMinute * MinutesPerHour * HoursPerDay;
+    private const int SecondsPerHour = SecondsPerMinute * MinutesPerHour;
+
+    private float gameTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    if (startAtCurrentTime)
+	    {
+            SetTime(DateTime.Now);
+	    }
+	    else
+	    {
+            SetTime(startingHour, startingMinute, startingSecond);
+	    }
+	}
+	
+	// Update is called once per frame
+
+    void Update ()
+	{
+	    UpdateTime();
+        Debug.Log("Gametime: " + gameTime);
+	    UpdateRotation();
+	}
+
+    public void SetTime(int hour, int minute, int second)
+    {
+        // Ensure the given time is valid.
+        if (hour >= 24 || minute >= 60 || second >= 60 
+            || hour < 0 || minute < 0 || second < 0)
+        {
+            Debug.LogError(string.Format("Invalid time set! Cannot set to {0}:{1}:{2}.", hour, minute, second));
+            return;
+        }
+
+        gameTime = hour * SecondsPerHour + minute * SecondsPerMinute + second;
+    }
+
+    public void SetTime(DateTime time)
+    {
+        SetTime(time.Hour, time.Minute, time.Second);
+    }
+
+    private void UpdateTime()
+    {
+        // Progress time.
+        gameTime += Time.deltaTime * timeProgression;
+
+        // If a full day has passed, reset the clock.
+        gameTime -= gameTime > SecondsPerDay ? SecondsPerDay : 0;
+    }
+
+    private void UpdateRotation()
+    {
+        transform.rotation = Quaternion.AngleAxis(Mathf.Lerp(0f, 360f, gameTime / SecondsPerDay), Vector3.forward);
+    }
+}
